3ce8b70ef97af1d08ab2411ab14b195ddb618066
Nicolò Vescera <slayercandy@icloud.com>
2015-02-20 08:55:18 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,8 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define N 4
+#define ETA 18
 
 int main(int argc, char** argv) {
+    int array[N];
+    int i,maggiori,cont;
+    printf("Inserisci l'età di %d persone\n",N);
 
+    for(i=0;i<N;i++){
+    cont=i+1;
+    printf("Inserisci l'eta' della %da persona: \n",cont);
+    scanf("%d",&(array[i]));
+    }
+    maggiori=0;
+    for(i=0;i<N;i++){
+        if(array[i]>=ETA)
+            maggiori++;
+    }
+    printf("I maggiorenni sono: %d\n",maggiori);
     return (EXIT_SUCCESS);
 }
 
